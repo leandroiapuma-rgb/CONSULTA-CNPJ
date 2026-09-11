89338049af9504759dc9f918385e9e0843491649
leandroiapuma-rgb <leandroiapu.ma@gmail.com>
2026-09-10 23:07:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static(__dirname);
+app.use(express.static(__dirname));
 
 // ------------------------------------------------------------
 // GET /api/cnpj/:cnpj
